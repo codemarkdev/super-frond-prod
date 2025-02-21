0ec8d8d4cc5b0ef241a351f29d85535aa100750a
Adonay  Aragón <aaragon@trainingroots.xyz>
2025-02-21 12:39:52 -0600

    update invetarory

--- src/components/Inventario/FormProducto.vue
+++ src/components/Inventario/FormProducto.vue
@@ -2,59 +2,69 @@
     <section class="box">
         <b-field grouped group-multiline>
             <b-field label="Código de barras" expanded>
-                <b-input type="number" icon="barcode" placeholder="Código de barras" v-model="producto.codigo"></b-input>
+                <b-input type="number" icon="barcode" placeholder="Código de barras"
+                    v-model="producto.code"></b-input>
             </b-field>
             <b-field label="Nombre" expanded>
-                <b-input placeholder="Nombre o descripción del producto" v-model="producto.nombre"></b-input>
+                <b-input placeholder="Nombre o descripción del producto" v-model="producto.name"></b-input>
             </b-field>
         </b-field>
+      
 
         <b-field grouped group-multiline>
             <b-field label="Precio compra" expanded>
-                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de compra" v-model="producto.precioCompra"></b-input>
+                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de compra"
+                    v-model="producto.purchasePrice"></b-input>
             </b-field>
 
             <b-field label="Precio venta" expanded>
-                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de venta" v-model="producto.precioVenta" :min="producto.precioCompra"></b-input>
+                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de venta"
+                    v-model="producto.salePrice" :min="producto.precioCompra"></b-input>
             </b-field>
-            
-            <b-field label="Existencia" expanded>
-                <b-numberinput min="1" type="is-info" placeholder="Existencia" v-model="producto.existencia">
+            <b-field label="Precio turista" expanded>
+                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de venta turista"
+                    v-model="producto.touristPrice" :min="producto.precioVenta"></b-input>
+            </b-field>
+  <b-field label="Existencia" expanded>
+                <b-numberinput min="1" type="is-info" placeholder="Existencia" v-model="producto.stock">
                 </b-numberinput>
             </b-field>
+       
         </b-field>
+   
         <br>
         <b-field grouped group-multiline>
             <b-field>
-                <b-switch v-model="producto.vendidoMayoreo"
-                type="is-info">
+                <b-switch v-model="producto.wholesaleSale" type="is-info">
                     ¿Vendido al mayoreo?
                 </b-switch>
             </b-field>
 
-            <b-field label="Precio mayoreo" expanded v-if="producto.vendidoMayoreo">
-                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de venta al mayoreo" v-model="producto.precioMayoreo" :min="producto.precioCompra"></b-input>
+            <b-field label="Precio mayoreo" expanded v-if="producto.wholesaleSale">
+                <b-input step="any" icon="currency-usd" type="number" placeholder="Precio de venta al mayoreo"
+                    v-model="producto.wholesalePrice" :min="producto.purchasePrice"></b-input>
             </b-field>
-            
-            <b-field label="Cantidad mayoreo" expanded v-if="producto.vendidoMayoreo">
-                <b-numberinput min="1" type="is-info" placeholder="Cantidad para aplicar mayoreo" v-model="producto.cantidadMayoreo">
+
+            <b-field label="Cantidad mayoreo" expanded v-if="producto.wholesaleSale">
+                <b-numberinput min="1" type="is-info" placeholder="Cantidad para aplicar mayoreo"
+                    v-model="producto.wholesaleQuantity">
                 </b-numberinput>
             </b-field>
         </b-field>
         <br>
         <b-field grouped group-multiline>
-            <b-field label="Selecciona una marca" >
-                <b-select placeholder="Marca" icon="tag-multiple" v-model="producto.marca">
+            <b-field label="Selecciona una marca">
+                <b-select placeholder="Marca" icon="tag-multiple" v-model="producto.brandId">
                     <option v-for="marca in marcas" :key="marca.id" :value="marca.id">
-                        {{ marca.nombreMarca }}
+                        {{ marca.brandName }}
                     </option>
                 </b-select>
             </b-field>
 
-            <b-field label="Selecciona una categoría" >
-                <b-select placeholder="Categoría" icon="archive" v-model="producto.categoria">
+            <b-field label="Selecciona una categoría">
+                <b-select placeholder="Categoría" icon="archive" v-model="producto.categoryId">
                     <option v-for="categoria in categorias" :key="categoria.id" :value="categoria.id">
-                        {{ categoria.nombreCategoria }}
+                        {{ categoria.categoryName }}
                     </option>
                 </b-select>
             </b-field>
@@ -62,96 +72,125 @@
         <br>
         <div class="buttons has-text-centered">
             <b-button type="is-primary" size="is-large" icon-left="check" @click="registrar">Registrar</b-button>
-            <b-button type="is-dark" size="is-large" icon-left="cancel" tag="router-link" to="/inventario">Cancelar</b-button>
+            <b-button type="is-dark" size="is-large" icon-left="cancel" tag="router-link"
+                to="/inventario">Cancelar</b-button>
         </div>
-        <errores-component :errores="mensajesError" v-if="mensajesError.length > 0"/>
+        <errores-component :errores="mensajesError" v-if="mensajesError.length > 0" />
     </section>
 </template>
-<script >
-    import HttpService from '../../Servicios/HttpService'
-    import Utiles from '../../Servicios/Utiles'
-    import ErroresComponent from '../Extras/ErroresComponent'
-    export default{
-        name: "FormProducto",
-        props: ['productoProp'],
-        components: { ErroresComponent },
+<script>
+import apiRequest from '../../Servicios/HttpService';
+import Utiles from '../../Servicios/Utiles'
+import ErroresComponent from '../Extras/ErroresComponent'
+export default {
+    name: "FormProducto",
+    props: ['productoProp'],
+    components: { ErroresComponent },
 
-        data:()=>({
-            categorias: [],
-            marcas: [],
-            producto: {
-                codigo: "",
-                nombre: "",
-                precioCompra: "",
-                precioVenta: "",
-                existencia: 0,
-                vendidoMayoreo: false,
-                precioMayoreo: "",
-                cantidadMayoreo: 0,
-                categoria: "",
-                marca:""
-            },
-            mensajesError: []
-        }),
-
-        mounted(){
-            this.obtenerCategorias()
-            this.obtenerMarcas()
-            this.producto = this.productoProp
-            this.producto.vendidoMayoreo = (this.productoProp.vendidoMayoreo === 1) ? true : false
-            this.producto.cantidadMayoreo = parseInt(this.productoProp.cantidadMayoreo)
+    data: () => ({
+        categorias: [],
+        marcas: [],
+        producto: {
+            code: "",
+            name: "",
+            purchasePrice: 0,
+            salePrice: 0,
+            touristPrice: 0,
+            stock: 0,
+            reservedStock: 0,
+            wholesaleSale: false,
+            wholesalePrice: 0,
+            wholesaleQuantity: 0,
+            brandId: "",
+            categoryId: ""
         },
+        mensajesError: []
+    }),
 
-        methods: {
+    mounted() {
+        this.obtenerCategorias()
+        this.obtenerMarcas()
+        if (this.productoProp) {
+            this.producto = { ...this.productoProp }
+            this.producto.wholesaleQuantity = parseInt(this.productoProp.wholesaleQuantity)
+            this.setSelectedValues();
+        }
+    },
 
-        registrar() {
+    methods: {
+        async registrar() {
             let producto = {
-                "Código": this.producto.codigo,
-                "Nombre": this.producto.nombre,
-                "Precio compra": this.producto.precioCompra,
-                "Precio venta": this.producto.precioVenta,
-                "Existencia": this.producto.existencia
+                "code": this.producto.code,
+                "name": this.producto.name,
+                "purchasePrice": this.producto.purchasePrice,
+                "salePrice": this.producto.salePrice,
+                "stock": this.producto.stock
             }
+
             this.mensajesError = Utiles.validarDatos(producto)
-            if(parseFloat(this.producto.precioVenta) < parseFloat(this.producto.precioCompra)){
+            if (parseFloat(this.producto.salePrice) < parseFloat(this.producto.purchasePrice)) {
                 this.mensajesError.push("El precio de venta debe ser mayor al precio compra")
             }
-            if(this.mensajesError.length > 0) return
+
+            if (this.mensajesError.length > 0) return
             this.$emit("registrado", this.producto)
-            this.producto= {
-                codigo: "",
-                nombre: "",
-                precioCompra: "",
-                precioVenta: "",
-                existencia: 0,
-                vendidoMayoreo: false,
-                precioMayoreo: "",
-                cantidadMayoreo: 0,
-                categoria: "",
-                marca:""
+            this.producto = {
+                code: "",
+                name: "",
+                purchasePrice: 0,
+                salePrice: 0,
+                touristPrice: 0,
+                stock: 0,
+                reservedStock: 0,
+                wholesaleSale: false,
+                wholesalePrice: 0,
+                wholesaleQuantity: 0,
+                brandId: "",
+                categoryId: ""
             }
         },
 
-        obtenerMarcas(){
-            let payload = {
-              accion: 'obtener'
-            }
-            HttpService.obtenerConConsultas('marcas.php', payload)
-            .then(marcas => {
-              this.marcas = marcas
-            })
-          },
 
-          obtenerCategorias(){
-            let payload = {
-              accion: 'obtener'
+        setSelectedValues() {
+
+        if (this.marcas.length > 0) {
+            const selectedMarca = this.marcas.find(marca => marca.id === this.producto.brandId);
+            if (selectedMarca) {
+                this.producto.brandId = selectedMarca.id;
             }
-            HttpService.obtenerConConsultas('categorias.php', payload)
-            .then(categorias => {
-              this.categorias = categorias
-            })
-          }
         }
 
+      
+        if (this.categorias.length > 0) {
+            const selectedCategoria = this.categorias.find(categoria => categoria.id === this.producto.categoryId);
+            if (selectedCategoria) {
+                this.producto.categoryId = selectedCategoria.id;
+            }
+        }
+    },
+
+        obtenerMarcas() {
+            apiRequest({
+                method: 'GET',
+                path: "brands"
+            })
+                .then(marcas => {
+                    this.marcas = marcas.data
+                    this.setSelectedValues();
+                })
+        },
+
+        obtenerCategorias() {
+            apiRequest({
+                method: "GET",
+                path: "categories"
+            })
+                .then(categorias => {
+                    this.categorias = categorias.data
+                    this.setSelectedValues();
+                })
+        }
     }
+
+}
 </script>
